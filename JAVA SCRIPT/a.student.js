d751b9d598d2d14550d0288484756237669d1b54
chanduP2211 <116336096+chanduP2211@users.noreply.github.com>
2022-12-29 18:51:02 +0530

    forth

--- JAVA SCRIPT/a.student.js
+++ JAVA SCRIPT/a.student.js
@@ -12,6 +12,11 @@ let main2 = document.querySelector(".main2");
 let update = document.querySelector(".up")
 let submit = document.querySelector(".submit")
 
+
+
+
+
+
 update.addEventListener('click',changing);
 
 let student = localStorage.getItem("student")
